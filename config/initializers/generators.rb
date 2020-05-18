1f743aa916657d3371c4f7e5e2de9b6b44781018
s.o <yzk612@gmail.com>
2020-05-18 12:05:16 +0000

    set preparation

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+Rails.application.config.jenerators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
+end
+#  CSS、JS、Helper関係のファイルの自動生成しない。
+#  Routerにルーティングを自動的に追加しない。
