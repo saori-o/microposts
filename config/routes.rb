8dfe5ebda63c869414c1c3d5f6cf00c5dc83063f
s.o <yzk612@gmail.com>
2020-05-21 05:09:28 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
+  
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
